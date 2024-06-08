cdbe864e474219f918173f7f9b9d149daa9b65f1
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2024-06-08 00:55:09 -0400

    Create the EventDispatcher with unit testing

---
+++ Dispatcher.Test/EventDispatcherTest.cs
@@ -0,0 +1,98 @@
+﻿namespace Dispatcher.Test;
+
+public class DispatcherTest
+{
+    private EventDispatcher dispatcher;
+
+    public DispatcherTest()
+    {
+        dispatcher = new EventDispatcher();
+        dispatcher.AddService(new Response());
+        dispatcher.AddHandler<PingHandler>();
+        dispatcher.AddHandler<TickHandler>();
+        dispatcher.AddListener<AlphaListener>();
+        dispatcher.AddListener<BravoListener>();
+    }
+
+    [Fact]
+    public async void TestDispatchingRequest()
+    {
+        var ping = new PingRequest();
+        var pong = (Response?)await dispatcher.DispatchAsync(ping);
+        Assert.Equal("Pong", pong?.Value);
+
+        var tick = new TickRequest();
+        var tock = await dispatcher.DispatchAsync(tick);
+        Assert.Equal("Tock", tock);
+    }
+
+    [Fact]
+    public async void TestDispatchingNotification()
+    {
+        var notification = new Subject();
+        await dispatcher.DispatchAsync(notification);
+        Assert.Equal(2, notification.Value);
+    }
+}
+
+public class Response
+{
+    public string Value { get; set; } = "";
+}
+
+public class PingRequest : Request
+{
+    public string Name { get; set; } = "Ping";
+}
+
+public class PingHandler : RequestHandler<PingRequest, Response>
+{
+    private Response response;
+
+    public PingHandler(Response response)
+    {
+        this.response = response;
+    }
+
+    public override Task<Response> InvokeAsync(PingRequest request, CancellationToken token = default)
+    {
+        response.Value = "Pong";
+        return Task.FromResult(response);
+    }
+}
+
+public class TickRequest : Request
+{
+    public string Name { get; set; } = "Tick";
+}
+
+public class TickHandler : RequestHandler<TickRequest, string>
+{
+    public override Task<string> InvokeAsync(TickRequest request, CancellationToken token = default)
+    {
+        return Task.FromResult("Tock");
+    }
+}
+
+public class Subject : Notification
+{
+    public int Value { get; set; } = 0;
+}
+
+public class AlphaListener : NotificationListener<Subject>
+{
+    public override Task InvokeAsync(Subject notification, CancellationToken token = default)
+    {
+        notification.Value++;
+        return Task.CompletedTask;
+    }
+}
+
+public class BravoListener : NotificationListener<Subject>
+{
+    public override Task InvokeAsync(Subject notification, CancellationToken token = default)
+    {
+        notification.Value++;
+        return Task.CompletedTask;
+    }
+}
---
+++ Dispatcher/src/EventDispatcher.cs
@@ -0,0 +1,136 @@
+﻿namespace Dispatcher;
+
+public class EventDispatcher
+{
+    private Dictionary<Type, object> serviceProvider = new();
+    private Dictionary<int, Type> handlerProvider = new();
+    private Dictionary<int, Dictionary<int, Type>> listenerProvider = new();
+
+    public EventDispatcher() { }
+
+    public EventDispatcher(object[] services)
+    {
+        foreach (var service in services)
+        {
+            AddService(service);
+        }
+    }
+
+    public void AddService(object service)
+    {
+        serviceProvider[service.GetType()] = service;
+    }
+
+    public void AddHandler<THandler>()
+    {
+        AddHandler(typeof(THandler));
+    }
+
+    public void AddHandler(Type handlerType)
+    {
+        var eventType = GetEventType(handlerType, typeof(RequestHandler<,>));
+        if (eventType == null)
+        {
+            throw new Exception("The given type '" + handlerType.Name + "' must extends the abstract request handler: " + typeof(RequestHandler<,>).FullName);
+        }
+
+        handlerProvider[eventType.GetHashCode()] = handlerType;
+    }
+
+    public void AddListener<TListener>()
+    {
+        AddListener(typeof(TListener));
+    }
+
+    public void AddListener(Type ListenerType)
+    {
+        var notificationType = GetEventType(ListenerType, typeof(NotificationListener<>));
+        if (notificationType == null)
+        {
+            throw new Exception("The given type '" + ListenerType.Name + "' must extends the abstract notification listener: " + typeof(NotificationListener<>).FullName);
+        }
+
+        if (!listenerProvider.ContainsKey(notificationType.GetHashCode()))
+        {
+            listenerProvider[notificationType.GetHashCode()] = new Dictionary<int, Type>();
+        }
+
+        listenerProvider[notificationType.GetHashCode()][ListenerType.GetHashCode()] = ListenerType;
+    }
+
+    private Type? GetEventType(Type handlerType, Type targetType)
+    {
+        var baseType = handlerType.BaseType;
+
+        if (baseType == null)
+        {
+            return null;
+        }
+
+        if (baseType.IsGenericType && baseType.GetGenericTypeDefinition() == targetType)
+        {
+            return baseType.GetGenericArguments()[0];
+        }
+
+        return GetEventType(baseType, targetType);
+    }
+
+    public async Task<object?> DispatchAsync(IEvent eventArgs, CancellationToken token = default)
+    {
+        if (handlerProvider.ContainsKey(eventArgs.GetType().GetHashCode()))
+        {
+            var handlerType = handlerProvider[eventArgs.GetType().GetHashCode()];
+            var handler = CreateInstance<IEventHandler>(handlerType);
+
+            return await handler.InvokeAsync(eventArgs, token);
+        }
+
+        if (listenerProvider.ContainsKey(eventArgs.GetType().GetHashCode()))
+        {
+            var listenerTypes = listenerProvider[eventArgs.GetType().GetHashCode()];
+
+            var tasks = new List<Task>();
+
+            foreach (var item in listenerTypes)
+            {
+                var listener = CreateInstance<IEventListener>(item.Value);
+                var task = listener.InvokeAsync(eventArgs, token);
+                tasks.Add(task);
+            }
+
+            foreach (var task in tasks)
+            {
+                await task;
+            }
+
+            return null;
+        }
+
+        throw new Exception("No registered handler or listener matches the given event: " + eventArgs.GetType().Name);
+    }
+
+    private TInstance CreateInstance<TInstance>(Type handlerType)
+    {
+        var constructor = handlerType.GetConstructors()[0];
+
+        var args = new List<object>();
+        var parameters = constructor.GetParameters();
+        foreach (var parameter in parameters)
+        {
+            var parameterType = parameter.ParameterType;
+            if (!serviceProvider.ContainsKey(parameterType))
+            {
+                throw new Exception("Cannot find a corresponding dependency of type '" + parameterType.Name + "' for the request handler: " + handlerType.FullName);
+            }
+
+            args.Add(serviceProvider[parameterType]);
+        }
+
+        if (args.Count > 0)
+        {
+            return (TInstance)Activator.CreateInstance(handlerType, args.ToArray())!;
+        }
+
+        return (TInstance)Activator.CreateInstance(handlerType)!;
+    }
+}
